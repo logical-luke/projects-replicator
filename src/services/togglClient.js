3cd5557b28d349ac2639dfea2e610f7df7e48b88
Lucas Ziaja <luke@logical-luke.com>
2021-05-01 23:54:15 +0200

    :sparkles: Add listing workspaces and user projects

---
+++ src/services/togglClient.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const api = axios.create({
+    baseURL: "https://api.track.toggl.com/api/v8",
+    timeout: 30000,
+});
+
+api.interceptors.request.use(
+    async (config) => {
+        const basicAuthCredentials = Buffer.from(process.env.TOGGL_API_KEY + ":" + "api_token").toString('base64');
+        config.headers.common["Authorization"] = "Basic " + basicAuthCredentials;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+const listUserProjects = async () => {
+    const workspaces = await listWorkspaces();
+    let data = [];
+    if (workspaces) {
+        let workspacesRequests = [];
+        workspaces.forEach((workspace) => {
+            workspacesRequests.push(
+                api.get(
+                    `/workspaces/${workspace.id}/projects`).then((response) => {
+                    return response.data
+                })
+            );
+        })
+        await Promise.all(workspacesRequests).then((responses) => {
+            responses.forEach((projects) => {
+                data = data.concat(projects);
+            })
+        })
+    }
+    return data;
+}
+
+const listWorkspaces = async () => {
+    let data;
+    await api.get('/workspaces').then((response) => {
+        data = response.data;
+    });
+    return data;
+}
+
+module.exports = {
+    listUserProjects: listUserProjects,
+    listWorkspaces: listWorkspaces
+};
